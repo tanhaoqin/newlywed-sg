57bb28ac76c4a5e8ac0bd86ba91db7a69db4bcc7
tanhaoqin <tanhq646@gmail.com>
2016-04-07 17:46:54 +0800

    added data

---
+++ app/data.js
@@ -0,0 +1,8 @@
+export const data = {
+  	Schools: {initialWeight: 9, active: true},
+	Hospitals: {initialWeight: 5, active: true},
+	Hello: {initialWeight: 7, active: false },
+	World: {initialWeight: 3, active: true},
+	David: {initialWeight: 3, active: true},
+	XiaoShuang: {initialWeight: 3, active: true}
+};
